7b49cd170f3a14589d3a8ec6de21bac4a38cdbe9
lilly0129@gmail.com <lilly0129@gmail.com@6a9a5cec-5951-9890-4b92-2ca83ee5861a>
2013-05-25 00:10:08 +0000

    Added more documentations to OnTaskCompleted.java

--- trunk/ExpenseTracker/src/com/example/expensetracker/OnTaskCompleted.java
+++ trunk/ExpenseTracker/src/com/example/expensetracker/OnTaskCompleted.java
@@ -1,5 +1,9 @@
 package com.example.expensetracker;
 
+/**
+ * OnTaskCompleted is a simple interface that LoginActivity,RecordActivity 
+ * and NfcActivity use to indicate the completion of the AsyncTask.
+ */
 public interface OnTaskCompleted {
 	void onTaskCompleted();
 
